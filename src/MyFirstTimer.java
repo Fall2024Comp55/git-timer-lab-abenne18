047dd9264e8c82a68c71a7bb96e0af25455c0e7a
alexshome <alexshome@dhcp-10-15-164-26.wireless.pacific.edu>
2024-10-05 23:57:18 -0700

    Timer needs to be created first with 2 arguments: a set time (1000) and (this). It is started with start(), then with an actionPerformed method, you can count it. You may also stop() or setInitialDelay().

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,11 +1,19 @@
-import acm.graphics.*;
-import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.Timer;
+
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
 	private GLabel myLabel;
+	private int x;
+	private int numTimes = 0;
+	private Timer myFirstTimer = new Timer(1000, this);
 
 	public void init() {
 		setSize(PROGRAM_WIDTH, PROGRAM_HEIGHT);
@@ -14,8 +22,31 @@ public class MyFirstTimer extends GraphicsProgram {
 	
 	public void run() {
 		myLabel = new GLabel("# of times called?", 0, 100);
+		
 		add(myLabel);
+		
+		//Timer myFirstTimer = new Timer(1000, this);
+        addMouseListeners();
+        myFirstTimer.start();
+
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	    //TODO add what we want to do every two seconds
+		if (numTimes < 10) {
+			numTimes += 1;
+			myLabel.setLabel("times called? " + numTimes);
+			x += 5;
+	        myLabel.setLocation(x, 100);
+		}
+		else {
+			myFirstTimer.stop();
+		}
+        
+		
+	}
+	
 	
 	public static void main(String[] args) {
 		new MyFirstTimer().start();
